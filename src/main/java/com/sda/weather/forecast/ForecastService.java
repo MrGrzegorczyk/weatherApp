e10d31ab297583a9e6b765a2b76ddf05a953ad4f
Michal Paukszto <michal.paukszto@gmail.com>
2022-01-22 16:05:53 +0100

    last improvements

--- src/main/java/com/sda/weather/forecast/ForecastService.java
+++ src/main/java/com/sda/weather/forecast/ForecastService.java
@@ -1,17 +1,22 @@
 package com.sda.weather.forecast;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+@Slf4j
+@RequiredArgsConstructor
 public class ForecastService {
 
     private static final String APP_KEY_ID = "1766fdc82c622688913c1bb885b9bd94";
     private static final String URL = "https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s&units=metric";
+
+    private final ObjectMapper objectMapper;
 
     Forecast getForecast(Long locationId, Integer period) {
         // todo develop this method
@@ -21,19 +26,17 @@ public class ForecastService {
     }
 
     ForecastClientResponse getForecastFromOpenweather(double longitude, double latitude) {
+        log.info("wywolano getForecastFromOpenweather");
         try {
-            String path = String.format(URL, longitude, latitude, APP_KEY_ID);
-            HttpRequest httpRequest = HttpRequest.newBuilder()
+            var path = String.format(URL, longitude, latitude, APP_KEY_ID);
+            var httpRequest = HttpRequest.newBuilder()
                     .GET()
                     .uri(URI.create(path))
                     .build();
 
-            HttpClient httpClient = HttpClient.newHttpClient();
-            HttpResponse<String> httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
-            String responseBody = httpResponse.body();
-
-            ObjectMapper objectMapper = new ObjectMapper();
-            objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            var httpClient = HttpClient.newHttpClient();
+            var httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+            var responseBody = httpResponse.body();
             return objectMapper.readValue(responseBody, ForecastClientResponse.class);
         } catch (Exception e) {
             e.printStackTrace();
--- src/test/java/com/sda/weather/forecast/ForecastServiceTest.java
+++ src/test/java/com/sda/weather/forecast/ForecastServiceTest.java
@@ -1,5 +1,7 @@
 package com.sda.weather.forecast;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -7,11 +9,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ForecastServiceTest {
 
     @Test
-    void getForecastFromOpenweather_returnsProperResponse(){
+    void getForecastFromOpenweather_returnsProperResponse() {
         // given
-        ForecastService forecastService = new ForecastService();
+        var objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        var forecastService = new ForecastService(objectMapper);
         // when
-        ForecastClientResponse result = forecastService.getForecastFromOpenweather(54.0, 18.0);
+        var result = forecastService.getForecastFromOpenweather(54.0, 18.0);
         // then
         assertThat(result).isNotNull();
     }
