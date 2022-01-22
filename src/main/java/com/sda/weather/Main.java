eb34ecd7f28828637bdf38730848f41830e50ee5
Marcin <grzegorczykmarcin4@gmail.com>
2022-01-22 14:05:39 +0100

    Small test changes

--- src/main/java/com/sda/weather/Main.java
+++ src/main/java/com/sda/weather/Main.java
@@ -23,5 +23,6 @@ public class Main {
 
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        //something
     }
 }
